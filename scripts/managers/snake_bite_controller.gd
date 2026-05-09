e7927a8abfc87f2d1a779eec409f3506ae1785c2
Aleksandr Churakov <Chur10@mail.ru>
2026-05-09 16:07:02 +0200

    change snake animation and bite flow

--- scripts/managers/snake_bite_controller.gd
+++ scripts/managers/snake_bite_controller.gd
@@ -2,11 +2,13 @@ extends Node
 class_name SnakeBiteController
 
 @onready var snake_spawn: Node3D = get_node("../../SnakeTrigger")
+@onready var snake_run_point: Node3D = get_node("../../SnakeRunPoint")
 @onready var poison_manager: Node = $"../PoisonManager"
 @onready var camera: Camera3D = get_node("../../Player/CameraMount/Camera3D")
 @onready var snake_scene: PackedScene = preload("res://scenes/snake.tscn")
 @onready var snake_instance: Node3D
 
+@export var snake_escape_duration: float = 1.5
 @export var bite_duration: float = 2.0
 @export var camera_turn_duration: float = 0.5
 
@@ -33,7 +35,8 @@ func _run_bite_sequence() -> void:
 	await _play_bite_effects()
 	await _look_at_bite_point()
 	await _wait_for_bite_phase()
-	await _remove_snake()
+	await _detach_snake_and_watch()
+	_remove_snake()
 	await _restore_camera()
 
 
@@ -45,17 +48,23 @@ func _spawn_snake() -> void:
 
 
 func _look_at_bite_point() -> void:
+	print("_look_at_bite_point()")
 	var bite_point := _player.get_node("SnakeBitePoint") as Node3D
 	_original_camera_transform = camera.global_transform
 	_player.set_control_enabled(false)
 
 	var target_transform := camera.global_transform.looking_at(bite_point.global_position, Vector3.UP)
 	await _tween_camera_basis(camera.global_transform.basis, target_transform.basis)
+	print("ended")
 
 
 func _restore_camera() -> void:
+	print("_restore_camera()")
+
 	await _tween_camera_basis(camera.global_transform.basis, _original_camera_transform.basis)
 	_player.set_control_enabled(true)
+	print("ended()")
+
 
 
 func _tween_camera_basis(from_basis: Basis, to_basis: Basis) -> void:
@@ -71,14 +80,37 @@ func _tween_camera_basis(from_basis: Basis, to_basis: Basis) -> void:
 
 
 func _play_bite_effects() -> void:
+	print("_play_bite_effects()")
 	# TODO: bite UI flash, screen shake, bite sound.
 	await get_tree().create_timer(0.5).timeout
+	print("ended")
 
 
 func _wait_for_bite_phase() -> void:
+	print("_wait_for_bite_phase()")
+	_snake.play_animation("ArmatureAction")
 	await get_tree().create_timer(bite_duration).timeout
+	print("ended")
 
 
 func _remove_snake() -> void:
-	await _snake.play_animation("SnakeArmature|Snake_Idle")
+	print("_remove_snake()")
 	_snake.queue_free()
+	print("ended()")
+
+
+func _detach_snake_and_watch() -> void:
+	print("_detach_snake_and_watch()")
+
+	_snake.play_animation("ArmatureAction_001")
+	await _snake.detach_and_escape_to_point(snake_run_point.global_position)
+
+	var elapsed := 0.0
+
+	while elapsed < snake_escape_duration and is_instance_valid(_snake):
+		camera.look_at(_snake.global_position, Vector3.UP)
+
+		await get_tree().process_frame
+		elapsed += get_process_delta_time()
+
+	print("ended")
--- scripts/snake.gd
+++ scripts/snake.gd
@@ -5,7 +5,12 @@ signal attached_to_player
 
 @export var move_speed: float = 30.0
 @export var attach_distance: float = 0.5
+@export var escape_speed: float = 4.0
 
+var is_escaping: bool = false
+var escape_target_position: Vector3
+
+@onready var head_point: Node3D = $HeadPoint
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var target: Node3D
@@ -13,6 +18,21 @@ var is_attached: bool = false
 
 
 func _physics_process(delta: float) -> void:
+	if is_escaping:
+		var run_target := escape_target_position
+		run_target.y = global_position.y
+
+		var direction := run_target - global_position
+
+		if direction.length() < 0.1:
+			is_escaping = false
+			return
+
+		global_position += direction.normalized() * escape_speed * delta
+		_face_head_towards(run_target)
+		return
+
+
 	if is_attached or target == null:
 		return
 
@@ -29,6 +49,43 @@ func attach_to_player() -> void:
 	global_transform = attach_point.global_transform
 	is_attached = true
 	attached_to_player.emit()
+
+
+func _face_head_towards(point: Vector3) -> void:
+	var head_forward := -head_point.global_transform.basis.z
+	var desired_forward := (point - head_point.global_position).normalized()
+
+	if desired_forward.length() == 0.0:
+		return
+
+	var rotation_axis := head_forward.cross(desired_forward)
+
+	if rotation_axis.length() == 0.0:
+		return
+
+	var angle := head_forward.angle_to(desired_forward)
+	global_rotate(rotation_axis.normalized(), angle)
+
+
+func detach_and_escape_to_point(point: Vector3) -> void:
+	var old_global_transform := global_transform
+
+	reparent(get_tree().current_scene, true)
+	global_transform = old_global_transform
+
+	target = null
+	is_attached = false
+	is_escaping = false
+
+	var drop_target := global_position
+	drop_target.y = point.y
+	escape_target_position = point
+
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", drop_target, 0.25)
+	await tween.finished
+
+	is_escaping = true
 
 
 func play_animation(animation_name: String) -> void:
