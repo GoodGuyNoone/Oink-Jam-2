716f0a5b652cb21dba2289f6bcf4894098f0b5b5
Aleksandr Churakov <Chur10@mail.ru>
2026-05-09 15:03:33 +0200

    fix dependencies

--- scripts/Data/snake_data.gd
+++ scripts/Data/snake_data.gd
@@ -2,6 +2,7 @@ extends Resource
 class_name SnakeData
 
 @export var name: String
+@export var snake_texture: Texture2D
 @export var required_cure: Array[String] = []
 @export var symptoms: Array[SymptomData] = []
 @export var bite_sprite: Texture2D
